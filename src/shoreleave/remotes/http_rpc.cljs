5c85c03681962daa7d702416af7b2791484d5fd8
Paul deGrandis <paul.degrandis@gmail.com>
2013-01-08 13:33:46 -0800

    A small cleanup of the rpc docs

--- src/shoreleave/remotes/http_rpc.cljs
+++ src/shoreleave/remotes/http_rpc.cljs
@@ -13,7 +13,7 @@
 ;; You can also set the resource url to something different (the default is "/_fetch"),
 ;; but you must use `(binding ...)` forms on the client-side
 ;;
-;; You will most likely use the macros to make these calls.  Here is an example:
+;; You will most likely use the `remote` macros to make these calls.  Here is an example:
 ;;
 ;;      (srm/rpc  (ping)  [pong-response]
 ;;          (js/alert pong-response]))
@@ -24,7 +24,15 @@
 
 (def ^:dynamic *remote-uri* "/_fetch")
 
-(defn remote-callback [remote params callback & extra-content]
+(defn remote-callback
+  "Call a remote-callback on the server.
+  Arguments:
+    remote - a string, the name of the remote on the server (eg. specified with a defremote)
+    params - a vector, the parameters to pass to the remote function
+    callback - a map or a function.  The map specifies {:on-success some-f, :on-error another-f}
+                otherwise, just a single function that will be called with on-complete is triggered
+    extra-content - varlist of key-value pairs, extra-content to merge into the payload/content map."
+  [remote params callback & extra-content]
   (if (map? callback)
     (let [{:keys [on-success on-error]} callback]
       (xhr/xhr [:post *remote-uri*]
@@ -50,18 +58,3 @@
                              (let [data (if (= data "") "nil" data)]
                                (callback (reader/read-string data))))))))
 
-; TODO I believe there is an error with the xhrManager getting back Clojure data, but I haven't confirmed it
-#_(defn remote-callback [remote params callback & extra-content]
-  (xhr/request [:post *remote-uri*]
-               :content (merge
-                          {:remote remote
-                           :params (pr-str params)}
-                          (apply hash-map extra-content))
-               :priority (dec priority/DEFAULT_PRIORITY_)
-               :on-success (when callback
-                             #(->> % :body
-                                (fn [data]
-                                  (let [data (if (= data "") "nil" data)]
-                                    (callback (reader/read-string data))))))
-               :on-error #(->> % :event js/console.log)))
- 
